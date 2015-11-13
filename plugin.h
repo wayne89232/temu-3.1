50a41343efd18705bb030aa83e41b2ac73b4947b
how2945ard <how2945ard@gmail.com>
2015-11-13 17:17:35 +0800

    test

--- plugin.h
+++ plugin.h
@@ -17,7 +17,7 @@ typedef struct plugin_interface_t{
     void *info_cmds; 
     long long cr3_addr;
     int taint_record_size;
-    void (*set_target_port)(int port_num);
+    void (*set_nic_target_port)(int port_num);
 
     // void (*taint_disk) (int size, int64_t sect_num, int flag,uint32_t paddr);
     // void (*nic_recv) (uint8_t * buf, int size, int cur_pos, int start, int stop);
