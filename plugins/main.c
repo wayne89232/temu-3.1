a32d18e4825353348181b7609c40e01abe50d9d2
how2945ard <how2945ard@gmail.com>
2015-11-13 17:30:17 +0800

    test

--- plugins/main.c
+++ plugins/main.c
@@ -43,7 +43,7 @@ static void test()
   printf("test\n");
 }
 
-static void set_nic_target_port(int port_num){
+static void do_set_nic_target_port(int port_num){
     target_port = port_num;
     printf("target port: %d\n", target_port);
 }
@@ -59,7 +59,7 @@ plugin_interface_t * init_plugin()
   my_interface.term_cmds = my_term_cmds;
 
   my_interface.test = test;
-  my_interface.set_nic_target_port = set_nic_target_port;
+  my_interface.set_nic_target_port = do_set_nic_target_port;
   return &my_interface;
 }
 
