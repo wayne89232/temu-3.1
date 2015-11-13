f04125cd6daf016892c03dd2a2121442065686b9
how2945ard <how2945ard@gmail.com>
2015-11-13 15:36:12 +0800

    test

--- plugins/main.h
+++ plugins/main.h
@@ -2,7 +2,7 @@ typedef struct mon_cmd_t {
     const char *name;
     const char *args_type;
     void (*user_print)();
-    void (*temu)(const char *opaque);
+    void (*temu)(char *opaque);
     const char *params;
     const char *help;
     union {
