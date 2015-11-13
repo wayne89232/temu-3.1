78318b6b40c912e02e7e2ebea065af2d0b49830f
how2945ard <how2945ard@gmail.com>
2015-11-13 16:22:08 +0800

    test

--- plugins/main.c
+++ plugins/main.c
@@ -9,7 +9,9 @@
 static plugin_interface_t my_interface;
 FILE *my_log;
 static void monitor_user_noop(Monitor *mon, const QObject *data) { }
-static void temu_fake_function(Monitor *mon, const QObject *data) { }
+static void temu_fake_function(Monitor *mon, const QObject *data) { 
+  printf("temu_fake_function called\n");
+}
 
 static void set_nic_target_port(const char* port)
 {
@@ -24,9 +26,9 @@ static mon_cmd_t my_term_cmds[] = {
     .args_type  = "port:I",
     .params     = "port",
     .temu       = set_nic_target_port,
-    .user_print = monitor_user_noop,
+    .user_print = temu_fake_function,
     .help       = "set nic_target_port",
-    .mhandler.cmd = set_nic_target_port
+    .mhandler.cmd = temu_fake_function
   },
   {NULL, NULL},
 };
