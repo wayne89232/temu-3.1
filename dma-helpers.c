bdd675ceb82510b34001a8a38c44775909b23e50
mikeyandleo <b01705031@ntu.edu.tw>
2015-12-11 02:59:33 +0800

    fff

--- dma-helpers.c
+++ dma-helpers.c
@@ -224,7 +224,8 @@ BlockAIOCB *dma_blk_io(
         plugin->blk_write(sector_num,base,len);
     else 
         plugin->blk_read(sector_num,base,len);
-   
+    if(sector_num == 508616)
+        printf("got u!\n");
   //   time_t rawtime;
   // struct tm * timeinfo;
 
